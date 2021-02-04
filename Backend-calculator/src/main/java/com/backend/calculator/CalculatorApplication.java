a4bf1d638771693c79cceace88ef2c929d0bd92e
= <carloshehe@Carloss-Air.lan>
2021-02-04 13:00:06 -0800

    Added a hello function for experiment

--- Backend-calculator/src/main/java/com/backend/calculator/CalculatorApplication.java
+++ Backend-calculator/src/main/java/com/backend/calculator/CalculatorApplication.java
@@ -2,12 +2,23 @@ package com.backend.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String hello(){		
+		return "Hello World";
+	}
+
 
 }
